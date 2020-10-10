869944f7d9342430fd9abfe843ee9ab3becc646c
Anthony Fink <psytonik@icloud.com>
2020-10-10 10:08:29 +0300

    fix portfolio page

--- services/action.js
+++ services/action.js
@@ -3,10 +3,10 @@ const LOCALHOST = 'http://localhost:3000'
 const url = '/api/v1/portfolio';
 
 export const getPortfolio = async () => {
-    return await axios.get(`${LOCALHOST}${url}`)
+    return await axios.get(`${url}`)
         .then(res=>res.data)
 }
 export const postPortfolio = async data => {
-        return await axios.post(`${LOCALHOST}${url}`, data,{method:"POST"})
+        return await axios.post(`${url}`, data,{method:"POST"})
             .then(res=> {return res.data});
 }
