f35b83b94da969ce4f6f48da536dc8eb6bf5b0dc
Anthony Fink <psytonik@icloud.com>
2020-10-10 11:05:40 +0300

    add features for pwa

--- pages/_app.js
+++ pages/_app.js
@@ -27,6 +27,7 @@ const MyApp = ({ Component, pageProps }) => {
               <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
               <meta charSet="UTF-8"/>
               <meta name="description" content="Anthony Fink CV site with blog"/>
+              <meta name="theme-color" content="#3bace5"/>
               <title>Anthony Fink CV</title>
           </Head>
           <Component {...pageProps}/>
