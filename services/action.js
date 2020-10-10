738c93c357717dd1c3dcfbf9c85762f5e117804f
Anthony Fink <psytonik@icloud.com>
2020-10-10 10:29:00 +0300

    checks

--- services/action.js
+++ services/action.js
@@ -1,12 +1,20 @@
 import axios from "axios";
-const LOCALHOST = 'http://localhost:3000'
+import useSWR from "swr";
+const Host = 'https://anthonyfink-cv.herokuapp.com'
 const url = '/api/v1/portfolio';
+import {fetcher} from "./index";
 
 export const getPortfolio = async () => {
-    return await axios.get(`${url}`)
+    return await axios.get(`${Host}${url}`)
         .then(res=>res.data)
 }
 export const postPortfolio = async data => {
-        return await axios.post(`${url}`, data,{method:"POST"})
+        return await axios.post(`${Host}${url}`, data,{method:"POST"})
             .then(res=> {return res.data});
 }
+
+// export const useGetPortfolio = (id) => {
+//     const { data, error, ...rest} = useSWR(id ? `/api/v1/portfolios/${id}` : null, fetcher);
+//     return { data, error, loading: !data && !error, ...rest};
+// }
+// const createPortfolio = (data) => axios.post('/api/v1/portfolios', data);
